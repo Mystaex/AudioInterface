1a6faaaebc0cc04c1fc1e97d4e3c1ce57658b9f4
Ethan Chase <ethanchase14@gmail.com>
2021-11-15 22:28:12 -0600

    Cleaned up code and commented

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from pydub import AudioSegment
-import sys
-sys.path.append('path/to/ffmpeg')
 
 root = tk.Tk()
 
-root.columnconfigure(0, weight = 1)
-root.columnconfigure(1, weight = 6)
-root.columnconfigure(2, weight = 1)
-root.columnconfigure(3, weight = 2)
-
+#Types of files that can be converted
 fileTypes = [
     "wav",
     "flac",
@@ -19,9 +13,18 @@ fileTypes = [
     "mp3",
 ]
 
-for x in range(4):
-    root.rowconfigure(x, weight = 1)
+#Functions
 
+#Configures the grid columns and rows
+def gridConfig():
+    root.columnconfigure(0, weight = 1)
+    root.columnconfigure(1, weight = 6)
+    root.columnconfigure(2, weight = 1)
+    root.columnconfigure(3, weight = 2)
+    for x in range(4):
+        root.rowconfigure(x, weight = 1)
+
+#Functionality for the submit button
 def submit():
     print(filename+' Submitted!')
     print("Type: "+defDrop.get())
@@ -56,38 +59,48 @@ def browseFiles():
     fileExt = fileKeys[1]
     extLabel.config(text = '.'+fileExt)
 
+#Lays out items in the grid
+def defineGUI():
+    gridConfig()
+    fileEntryLabel.grid(column = 0, row = 0, sticky = 'e')
+    fileExtLabel.grid(column = 2, row = 0, sticky = 'e')
+    convExtLabel.grid(column = 2, row = 2, sticky = 'ne')
 
+    fileEntry.grid(column = 1, row = 0, sticky = 'w')
+    extLabel.grid(column = 3, row = 0, sticky = 'w')
+    fileButton.grid(column = 1, row = 1, sticky = 'nw')
+    extDrop.grid(column = 3, row = 2, sticky = 'nw')
+    submitButton.grid(column = 1, row = 2, sticky = '')
+
+
+
+#          #
+#   MAIN   #
+#          #
+
+#Define GUI frame attributes
 root.title('Audio Converter')
 root.geometry('600x300+50+50')      #widthxheight+-x+-y
 root.resizable(False,False)         #widthResize, heightResize
 root.attributes('-alpha', 1.0)    #0.0 is transparent, 1.0 is opaque
 
+#Define buttons and labels
 fileEntry = ttk.Entry(root, width = 30)
-
 extLabel = ttk.Label(root, width = 6)
+fileButton = ttk.Button(root, text = 'Browse Files', command = browseFiles, width = 10)
+submitButton = ttk.Button(root, text = 'submit', command = submit, width = 10)
 
+convExtLabel = tk.Label(root, text = 'to:', bg = 'white')
+fileExtLabel = tk.Label(root, text = 'from:', bg = 'white')
+fileEntryLabel = tk.Label(root, text = 'filename:', bg = 'white')
+
+#Define dropdown menu
 defDrop = tk.StringVar()
 defDrop.set("wav")
 extDrop = tk.OptionMenu(root, defDrop, *fileTypes)
 
-convEntry = ttk.Entry(root, width = 6)
+defineGUI()
 
-fileButton = ttk.Button(root, text = 'Browse Files', command = browseFiles, width = 10)
-
-submitButton = ttk.Button(root, text = 'submit', command = submit, width = 10)
-
-fileEntryLabel = tk.Label(root, text = 'filename:', bg = 'white')
-fileEntryLabel.grid(column = 0, row = 0, sticky = 'e')
-fileExtLabel = tk.Label(root, text = 'from:', bg = 'white')
-fileExtLabel.grid(column = 2, row = 0, sticky = 'e')
-convExtLabel = tk.Label(root, text = 'to:', bg = 'white')
-convExtLabel.grid(column = 2, row = 2, sticky = 'ne')
-
-fileEntry.grid(column = 1, row = 0, sticky = 'w')
-extLabel.grid(column = 3, row = 0, sticky = 'w')
-fileButton.grid(column = 1, row = 1, sticky = 'nw')
-extDrop.grid(column = 3, row = 2, sticky = 'nw')
-submitButton.grid(column = 1, row = 2, sticky = '')
-
-
+#Needed for GUI work
+#End of main program
 root.mainloop()
